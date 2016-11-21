f1dc3bf1d6f8cc87e9e888b312843f9a0dbda168
yuyuepeng <616250176@qq.com>
2016-11-21 11:42:48 +0800

    qqqqqq

---
+++ Pods/Target Support Files/Pods-TableViewTryTests/Pods-TableViewTryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableViewTryTests : NSObject
+@end
+@implementation PodsDummy_Pods_TableViewTryTests
+@end
---
+++ Pods/Target Support Files/Pods-TableViewTryUITests/Pods-TableViewTryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableViewTryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TableViewTryUITests
+@end
